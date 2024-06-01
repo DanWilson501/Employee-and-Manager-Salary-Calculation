8d4121d6abd5723204cb90ef233dfa70aec98d91
Daniel Wilson <danwilson501@gmail.com>
2024-05-31 23:31:20 -0400

    Calculated and logged the data set of managers for their annual salaries plus bonuses.

--- employee-manager-salary.js
+++ employee-manager-salary.js
@@ -24,3 +24,12 @@ class Manager extends Employee {
         return baseAnnualSalary + bonus;
     }
 }
+
+const manager1 = new Manager('Bob Ross', 6600, 'Digital Marketing');
+const manager2 = new Manager('Debbie Little', 7205, 'Finance');
+
+console.log(`Manager 1`);
+console.log(`Name: ${manager1.name}, Monthly Salary: ${manager1.salary} Department: ${manager1.department}\n Annual Salary: ${manager1.annualSalary()}`);
+
+console.log(`Manager 2`);
+console.log(`Name: ${manager2.name}, Monthly Salary: ${manager2.salary} Department: ${manager2.department}\n Annual Salary: ${manager2.annualSalary()}`);
